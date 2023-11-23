db506f342e4e9052c9b043d7e64c31c27fadcc0a
Guven Karanfil <guven.karanfil@smartface.io>
2023-11-23 03:55:54 +0300

    fix typo setup file and and bump jest version with approirate configurations for prepare setup with react-navigation library

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,12 @@ module.exports = {
   preset: '@testing-library/react-native',
   setupFilesAfterEnv: [
     './node_modules/@testing-library/jest-native/extend-expect',
-    './jest.config.js',
+    './jest.setup.js',
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation)',
+
+    // For pnpm you need to use inlcude `(?!(?:.pnpm/)?` part like this:
+    // 'node_modules/(?!(?:.pnpm/)?((jest-)?@?react-native|@react-native-community|@react-navigation))',
   ],
 };
---
+++ jest.setup.js
@@ -0,0 +1,19 @@
+import {jest} from '@jest/globals';
+
+import '@testing-library/react-native/extend-expect';
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+jest.mock('@react-navigation/native', () => {
+  const actualNav = jest.requireActual('@react-navigation/native');
+  return {
+    ...actualNav,
+    useNavigation: () => ({
+      navigate: jest.fn(),
+      dispatch: jest.fn(),
+    }),
+    useRoute: jest.fn(),
+    useIsFocused: jest.fn(),
+  };
+});
