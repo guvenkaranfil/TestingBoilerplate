4f6d3f3257f5ad21aff4a29fb1c5e1f47f4dba3d
Guven Karanfil <guven.karanfil@smartface.io>
2023-12-13 21:47:11 +0300

    make sure 'List' component renders correctly

---
+++ __tests__/List.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {render} from '../.jest/helper/testUtils';
+
+const List = () => {
+  return null;
+};
+
+describe('List component with search functionality use cases', () => {
+  test('should List component render correctly', () => {
+    render(<List />);
+  });
+});
