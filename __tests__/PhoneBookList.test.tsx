b89a64d93615b847dd5f17c9f911cec7563c3f83
guvenkaranfil <guvenkaranfil235@gmail.com>
2023-12-27 01:48:33 +0300

    should 'PhoneBookList' component render correctly

---
+++ __tests__/PhoneBookList.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {render} from '../.jest/helper/testUtils';
+
+const PhoneBookList = () => {
+  return null;
+};
+
+describe('PhoneBook List with Search functionality', () => {
+  test('should component render correctly', () => {
+    render(<PhoneBookList />);
+  });
+});
