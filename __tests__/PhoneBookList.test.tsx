d31796802d84850717873bc4ee0cb14c1098ecec
guvenkaranfil <guvenkaranfil235@gmail.com>
2023-12-27 20:45:52 +0300

    should be able to type to input

--- __tests__/PhoneBookList.test.tsx
+++ __tests__/PhoneBookList.test.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import {render} from '../.jest/helper/testUtils';
+import React, {useState} from 'react';
+import {TextInput} from 'react-native';
 
 const PhoneBookList = () => {
-  return null;
+  const [name, setname] = useState('');
+
+  return (
+    <TextInput placeholder="Search Name" value={name} onChangeText={setname} />
+  );
 };
 
+import {render, screen, userEvent} from '../.jest/helper/testUtils';
 describe('PhoneBook List with Search functionality', () => {
   test('should component render correctly', () => {
     render(<PhoneBookList />);
+  });
+
+  test('should be able to type to input', async () => {
+    render(<PhoneBookList />);
+
+    const input = screen.getByPlaceholderText('Search Name');
+    expect(input.props.value).toBe('');
+
+    await userEvent.type(input, 'test');
+    expect(input.props.value).toBe('test');
+
+    await userEvent.clear(input);
+    expect(input.props.value).toBe('');
+
+    await userEvent.type(input, 'test with clear');
+    expect(input.props.value).toBe('test with clear');
   });
 });
