9f8df8f3e34a6e5c6e8cb4c8648a1202ca368600
guvenkaranfil <guvenkaranfil235@gmail.com>
2024-01-08 01:04:43 +0300

    add scenario to MenuEdit performance test

--- .perf-test.js/MenuEdit.perf-test.tsx
+++ .perf-test.js/MenuEdit.perf-test.tsx
@@ -5,9 +5,18 @@ import MenuEdit from '../app/tabs/menuEdit';
 import AllProviders from '../.jest/helper/AllProviders';
 
 import {MOCK_MENU_ITEMS} from '../__tests__/MenuEdit.test';
+import {fireEvent, screen} from '@testing-library/react-native';
 
 test('MenuEdit Component Performance', async () => {
+  const scenario = async (componentScreen: typeof screen) => {
+    const checkedItem = MOCK_MENU_ITEMS.filter(item => item.isActive)[0];
+    const checkedItemElement = componentScreen.getByTestId(checkedItem.id);
+
+    await fireEvent.press(checkedItemElement);
+  };
+
   await measurePerformance(<MenuEdit menus={MOCK_MENU_ITEMS} />, {
+    scenario,
     wrapper: AllProviders,
   });
 });
